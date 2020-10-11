98fa38cb5192a307bb7bcf410931f22c9199fdb0
Guntur <guntur@symboinsurance.com>
2020-10-11 17:36:17 +0700

    Add mouse key for planck

--- keyboards/planckgkh/keymaps/default/keymap.c
+++ keyboards/planckgkh/keymaps/default/keymap.c
@@ -2,18 +2,20 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+  // LAYER 0
 	KEYMAP(
-  //,-----------------------------------------------------------------------------------------------------------------------.
-	    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,          KC_BSPC, 
-  //,-------------+---------+--------+--------+--------+--------+-------+------+--------+--------+--------+-----------------|
-    LCTL_T(KC_ESC),     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN,          KC_QUOT, 
-  //,-------------+---------+--------+--------+--------+--------+-------+------+--------+--------+--------+-----------------|
-           KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,   RSFT_T(KC_ENT), 
-  //,-------------+---------+--------+--------+--------+--------+-------+------+--------+--------+--------+-----------------|
-	     MO(3),  KC_LCTL, KC_LALT, KC_LGUI,   MO(1),      KC_SPC,     MO(2), KC_LEFT, KC_DOWN,   KC_UP,           KC_RGHT
-  //,-------------+---------+--------+--------+--------+----------------+------+--------+--------+--------+-----------------|
+  //,---------------------------------------------------------------------------------------------------------------------------.
+	    KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,          KC_BSPC, 
+  //,-------------+-------------+--------+--------+--------+--------+-------+------+--------+--------+--------+-----------------|
+    LCTL_T(KC_ESC),  LT(4, KC_A),    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN,          KC_QUOT, 
+  //,-------------+-------------+--------+--------+--------+--------+-------+------+--------+--------+--------+-----------------|
+           KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,   RSFT_T(KC_ENT), 
+  //,-------------+-------------+--------+--------+--------+--------+-------+------+--------+--------+--------+-----------------|
+	     MO(3),      KC_LCTL, KC_LALT, KC_LGUI,   MO(1),      KC_SPC,     MO(2),   MO(5), KC_TRNS, KC_TRNS,          KC_TRNS
+  //,-------------+-------------+--------+--------+--------+----------------+------+--------+--------+--------+-----------------|
 		),
 
+  // LAYER 1
 	KEYMAP(
   //,--------------------------------------------------------------------------------------------------------------------------------.
 	   KC_TILD,  KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  KC_CIRC,        KC_AMPR,       KC_ASTR, KC_LPRN, KC_RPRN,   KC_TRNS, 
@@ -26,8 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------+---------+--------+--------+--------+------------------+---------------+--------------+--------+--------+---------|
 		),
 
+  // LAYER 2
 	KEYMAP(
-  //,------------------------------------------------------------------------------------------------------------------.
+  //,---------------------------------------------------------------------------------------------------------------------.
 	    KC_GRV,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS, 
   //,-------------+---------+--------+--------+--------+--------+-------+---------+--------+--------+--------+------------|
 	    KC_DEL,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,    KC_BSLS, 
@@ -37,23 +40,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	     RESET,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU,    KC_MPLY),
   //,-------------+---------+--------+--------+--------+----------------+---------+--------+--------+--------+------------|
 
+  // LAYER 3
 	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7, KC_8,    KC_9,    KC_MINS,  KC_ASTR, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4, KC_5,    KC_6,    KC_PLUS,  KC_SLSH, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1, KC_2,    KC_3,    KC_EQL,   KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 		  KC_ENT, 		 KC_0, KC_DOT,  KC_TRNS, KC_ENT,   KC_TRNS),
+  //,---------------------------------------------------------------------------------------------------------------------.
+	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_7,    KC_8,    KC_9, KC_MINS,     KC_ASTR, 
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_4,    KC_5,    KC_6, KC_PLUS,     KC_SLSH, 
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_1,    KC_2,    KC_3,  KC_EQL,     KC_TRNS, 
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_ENT, 	      KC_0,  KC_DOT, KC_TRNS,  KC_ENT,    KC_TRNS),
+  //,-------------+---------+--------+--------+--------+----------------+---------+--------+--------+--------+------------|
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
+  // LAYER 4
 	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+  //,---------------------------------------------------------------------------------------------------------------------.
+	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_MINS,     KC_ACL2, 
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS, 	   KC_ACL1,
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_BTN1,KC_BTN2,    KC_3,  KC_EQL,     KC_ACL0, 
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_ENT, 	      KC_0,  KC_DOT, KC_TRNS,  KC_ENT,    KC_TRNS),
+  //,-------------+---------+--------+--------+--------+----------------+---------+--------+--------+--------+------------|
+
+  // LAYER 5
+	KEYMAP(
+  //,---------------------------------------------------------------------------------------------------------------------.
+	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, 
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, KC_TRNS, 	   KC_TRNS,
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, 
+  //,-------------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+------------|
+      	   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_ENT, 	      KC_0,  KC_DOT, KC_TRNS,  KC_ENT,    KC_TRNS),
+  //,-------------+---------+--------+--------+--------+----------------+---------+--------+--------+--------+------------|
 
 	KEYMAP(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
